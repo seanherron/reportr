91438f1f5bd5f50d07ecd7fe076c69a93bac1ff4
Sean Herron <seanherron@gmail.com>
2014-01-07 10:27:18 -0500

    basic search

--- reportr/views.py
+++ reportr/views.py
@@ -1,7 +1,10 @@
 from flask import Flask, request, session, g, redirect, url_for, abort, render_template, flash
-from reportr import app
+from urlparse import parse_qs
+
 from pyelasticsearch import ElasticSearch
-from jinja_filters import _jinja2_filter_datetime
+
+from reportr import app
+from jinja_filters import *
 
 es = ElasticSearch(app.config['ES_URL'])
 
@@ -19,6 +22,23 @@ def show_all_reports(page):
     previous_page = page - 1
     reports = es.search('sender.senderorganization:"FDA-Public Use"', index=app.config['ES_INDEX'], es_from=search_from, size=app.config['RESULTS_PER_PAGE'])
     return render_template('show_all_reports.html', reports=reports["hits"]["hits"], first_page=first_page, starting_number=starting_number, ending_number=ending_number, count=reports["hits"]["total"], per_page=app.config['RESULTS_PER_PAGE'], next_page=next_page, previous_page=previous_page)
+
+@app.route('/', defaults={'page': 1})
+@app.route('/search/')
+def search_reports():
+    search = ""
+    #return str(parse_qs(request.query_string))
+    for param, term in parse_qs(request.query_string).iteritems():
+        if param == "medicinalproduct":
+            search+="patient.drug.medicinalproduct:%s" % str(term[0])
+   # search_from = page * app.config['RESULTS_PER_PAGE'] - app.config['RESULTS_PER_PAGE'] + 1
+  #  starting_number = search_from
+   # ending_number = search_from + app.config['RESULTS_PER_PAGE'] - 1
+   # next_page = page + 1
+  #  previous_page = page - 1
+    reports = es.search(search, index=app.config['ES_INDEX'])
+    return render_template('show_all_reports.html', reports=reports["hits"]["hits"])
+
 
 @app.route('/safetyreport/<safetyreportid>')
 def show_report(safetyreportid):
